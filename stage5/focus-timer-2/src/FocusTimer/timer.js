076fdd03c09adac0bd4f7356f2d1325ba6341104
Sergio Sanchez <ssanchezfilho@gmail.com>
2023-12-11 20:01:00 -0300

    feat: stage5 focus-timer 2

---
+++ stage5/focus-timer-2/src/FocusTimer/timer.js
@@ -0,0 +1,36 @@
+import state from './state.js';
+import * as el from './elements.js';
+import { reset } from './actions.js';
+
+export function countdown() {
+  clearTimeout(state.countdownId)
+
+  if (!state.isRunning) {
+    return
+  }
+
+  let minutes = Number(el.minutes.textContent)
+  let seconds = Number(el.seconds.textContent)
+
+  seconds--
+
+  if (seconds < 0) {
+    seconds = 59
+    minutes--
+  }
+
+  if (minutes < 0) {
+    reset()
+    // kitchenTimer.play()
+    return
+  }
+
+  updateDisplay(minutes, seconds)
+
+  state.countdownId = setTimeout(() => countdown(), 1000)
+}
+
+export function updateDisplay(minutes, seconds) {
+  minutes = minutes ?? state.minutes
+  seconds = seconds ?? state.seconds
+}
