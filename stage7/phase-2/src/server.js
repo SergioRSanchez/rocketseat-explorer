1206e332810087840b740a10bb5e8b3535574237
Sergio Sanchez <ssanchezfilho@gmail.com>
2023-12-19 15:40:57 -0300

    feat: stage7 phase2 - create routes

--- stage7/phase-2/src/server.js
+++ stage7/phase-2/src/server.js
@@ -1,25 +1,11 @@
-const express = require('express');
+const express = require("express");
+const routes = require("./routes");
 
 const app = express();
+app.use(express.json());
+app.use(routes);
 
 const PORT = 3333;
-
-app.get('/messages/:id/:user', (req, res) => {
-  const { id, user } = req.params;
-  res.send(`
-    Id: ${id}.
-    User: ${user}.
-  `);
-})
-
-app.get('/users', (req, res) => {
-  const { page, limit } = req.query;
-  res.send(`
-    Page: ${page}.
-    Limit: ${limit}.
-  `);
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}!`);
 })
