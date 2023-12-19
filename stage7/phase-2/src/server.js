02391786dd976491477f4849b76dbd3968337a82
Sergio Sanchez <ssanchezfilho@gmail.com>
2023-12-19 14:43:22 -0300

    feat: stage7 phase-2

--- stage7/phase-2/src/server.js
+++ stage7/phase-2/src/server.js
@@ -4,9 +4,21 @@ const app = express();
 
 const PORT = 3333;
 
-app.get('/', (req, res) => {
-  res.send('Hello, world');
+app.get('/messages/:id/:user', (req, res) => {
+  const { id, user } = req.params;
+  res.send(`
+    Id: ${id}.
+    User: ${user}.
+  `);
 })
+
+app.get('/users', (req, res) => {
+  const { page, limit } = req.query;
+  res.send(`
+    Page: ${page}.
+    Limit: ${limit}.
+  `);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}!`);
