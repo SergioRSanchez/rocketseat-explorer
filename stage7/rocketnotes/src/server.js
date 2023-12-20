cb12d21be2b909d0e316f0e73d071ce24de5a7a8
Sergio Sanchez <ssanchezfilho@gmail.com>
2023-12-20 16:38:22 -0300

    feat: create database

--- stage7/rocketnotes/src/server.js
+++ stage7/rocketnotes/src/server.js
@@ -2,12 +2,14 @@ require("express-async-errors");
 
 const express = require("express");
 const routes = require("./routes");
+const database = require("./database/sqlite")
 
 const AppError = require("./utils/AppError")
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+database();
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({
