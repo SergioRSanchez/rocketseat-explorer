19f0b394b8945a4c99cd168a57c24e44fd3d438a
Sergio Sanchez <ssanchezfilho@gmail.com>
2023-12-21 13:08:07 -0300

    feat: create user table function

--- stage7/rocketnotes/src/server.js
+++ stage7/rocketnotes/src/server.js
@@ -2,14 +2,15 @@ require("express-async-errors");
 
 const express = require("express");
 const routes = require("./routes");
-const database = require("./database/sqlite")
+const migrationsRUn = require("./database/sqlite/migrations")
 
 const AppError = require("./utils/AppError")
+
+migrationsRUn();
 
 const app = express();
 app.use(express.json());
 app.use(routes);
-database();
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({
