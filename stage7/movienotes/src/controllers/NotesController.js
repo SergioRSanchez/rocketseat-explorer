605d8d963aa435259b021478931c7962e74e770b
Sergio Sanchez <ssanchezfilho@gmail.com>
2023-12-28 15:03:33 -0300

    feat: knex create note

--- stage7/movienotes/src/controllers/NotesController.js
+++ stage7/movienotes/src/controllers/NotesController.js
@@ -1,2 +1,27 @@
 const knex = require("../database/knex");
 
+class NotesController {
+  async create(req, res) {
+    const { title, description, rating, tags } = req.body
+    const { user_id } = req.params
+
+    const [note_id] = await knex("notes").insert({
+      title,
+      description,
+      rating,
+      user_id
+    })
+
+    const tagInsert = tags.map(name => {
+      return {
+        note_id,
+        name,
+        user_id
+      }
+    })
+
+    await knex("tags").insert(tagInsert)
+
+    res.json()
+  }
+}
