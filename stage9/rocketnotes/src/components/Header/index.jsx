4d3b8c6d842309a1e19f5d5748f7b4f342f36439
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-01-03 20:43:32 -0300

    feat: create profile component

--- stage9/rocketnotes/src/components/Header/index.jsx
+++ stage9/rocketnotes/src/components/Header/index.jsx
@@ -1,9 +1,20 @@
-import { Container } from './styles';
+import { Container, Profile } from './styles';
+import { RiShutDownLine } from 'react-icons/ri'
 
 export function Header() {
   return (
     <Container>
+      <Profile>
+        <img
+          src="https://github.com/SergioRSanchez.png"
+          alt="Foto do usuário"
+        />
 
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Sergio R Sanchez</strong>
+        </div>
+      </Profile>
     </Container>
   )
 }
