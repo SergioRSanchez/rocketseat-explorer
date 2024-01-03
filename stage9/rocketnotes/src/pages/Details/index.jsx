c2ed498180e28625e7a75c2f8829dd367b8acad5
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-01-03 20:24:54 -0300

    feat: update Details page

--- stage9/rocketnotes/src/pages/Details/index.jsx
+++ stage9/rocketnotes/src/pages/Details/index.jsx
@@ -5,10 +5,6 @@ import { Button } from '../../components/Button'
 export function Details() {
   return (
     <Container>
-      <h1>Hello World!</h1>
-
-      <Button title="Login" loading />
-      <Button title="Cadastrar" />
       <Button title="Voltar" />
     </Container>
   )
