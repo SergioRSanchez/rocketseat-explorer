fd1ac4cb2d34c6b24d1dd32c23c49003291baa1c
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-01-03 20:59:59 -0300

    feat: create Section components

--- stage9/rocketnotes/src/pages/Details/index.jsx
+++ stage9/rocketnotes/src/pages/Details/index.jsx
@@ -1,13 +1,26 @@
-import { Container } from './styles';
+import { Container, Links } from './styles';
 
 import { Header } from '../../components/Header';
+import { Section } from '../../components/Section';
 import { Button } from '../../components/Button';
 
 export function Details() {
   return (
     <Container>
       <Header />
-      <Button title="Voltar" />
+
+      <Section
+        title="Links úteis"
+      >
+        <Links>
+          <li><a href="#">https://www.rocketseat.com.br/</a></li>
+          <li><a href="#">https://www.rocketseat.com.br/</a></li>
+        </Links>
+      </Section>
+
+      <Button
+        title="Voltar"
+      />
     </Container>
   )
 }
