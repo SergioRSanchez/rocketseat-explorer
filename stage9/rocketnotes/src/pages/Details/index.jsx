bf21acdc5c0f041f4440d512bb1a1cfdb62ea748
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-01-03 21:19:41 -0300

    feat: create Tag Component

--- stage9/rocketnotes/src/pages/Details/index.jsx
+++ stage9/rocketnotes/src/pages/Details/index.jsx
@@ -3,19 +3,23 @@ import { Container, Links } from './styles';
 import { Header } from '../../components/Header';
 import { Section } from '../../components/Section';
 import { Button } from '../../components/Button';
+import { Tag } from '../../components/Tag';
 
 export function Details() {
   return (
     <Container>
       <Header />
 
-      <Section
-        title="Links úteis"
-      >
+      <Section title="Links úteis" >
         <Links>
           <li><a href="#">https://www.rocketseat.com.br/</a></li>
           <li><a href="#">https://www.rocketseat.com.br/</a></li>
         </Links>
+      </Section>
+
+      <Section title="Marcadores" >
+        <Tag title="express" />
+        <Tag title="nodejs" />
       </Section>
 
       <Button
