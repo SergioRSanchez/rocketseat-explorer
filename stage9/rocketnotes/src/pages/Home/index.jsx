a26a6fb3b94071897817c65d546b36ae2b1a5777
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-01-05 11:56:39 -0300

    feat: create new notes section

--- stage9/rocketnotes/src/pages/Home/index.jsx
+++ stage9/rocketnotes/src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+import { FiPlus } from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 
 import { Header } from '../../components/Header';
@@ -22,7 +23,10 @@ export function Home() {
 
       <Content></Content>
 
-      <NewNote></NewNote>
+      <NewNote>
+        <FiPlus />
+        Criar notas
+      </NewNote>
     </Container>
   )
 }
