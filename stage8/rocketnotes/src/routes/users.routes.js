f0fa46990737b49b74d0277cefa12c308617a332
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-01-11 15:29:09 -0300

    feat: created route to upload avatar

--- stage8/rocketnotes/src/routes/users.routes.js
+++ stage8/rocketnotes/src/routes/users.routes.js
@@ -1,13 +1,20 @@
 const { Router } = require("express");
+const multer = require("multer");
 
+const uploadConfig = require("../configs/upload");
 const UsersController = require("../controllers/UsersController");
 const ensureAuthenticated = require("../middleware/ensureAuthenticated");
 
 const userRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 const userController = new UsersController()
 
 userRoutes.post("/", userController.create);
 userRoutes.put("/", ensureAuthenticated, userController.update);
+userRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) => {
+  console.log(request.file.filename);
+  return response.json();
+});
 
 
 
