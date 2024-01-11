9bfeeed1b23c40334ef6ccee8687879c0ca3fe7c
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-01-11 17:43:16 -0300

    feat: created route to get user avatar

--- stage8/rocketnotes/src/server.js
+++ stage8/rocketnotes/src/server.js
@@ -5,11 +5,13 @@ const routes = require("./routes");
 const migrationsRUn = require("./database/sqlite/migrations")
 
 const AppError = require("./utils/AppError")
+const uploadConfig = require("./configs/upload")
 
 migrationsRUn();
 
 const app = express();
 app.use(express.json());
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 app.use(routes);
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
