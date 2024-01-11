d5d77b7afc7313fcab8d2805debe06be8a7579d2
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-01-11 17:29:08 -0300

    feat: created UserAvatarController

--- stage8/rocketnotes/src/routes/users.routes.js
+++ stage8/rocketnotes/src/routes/users.routes.js
@@ -3,18 +3,17 @@ const multer = require("multer");
 
 const uploadConfig = require("../configs/upload");
 const UsersController = require("../controllers/UsersController");
+const UserAvatarController = require("../controllers/UserAvatarController");
 const ensureAuthenticated = require("../middleware/ensureAuthenticated");
 
 const userRoutes = Router();
 const upload = multer(uploadConfig.MULTER);
-const userController = new UsersController()
+const userController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 userRoutes.post("/", userController.create);
 userRoutes.put("/", ensureAuthenticated, userController.update);
-userRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) => {
-  console.log(request.file.filename);
-  return response.json();
-});
+userRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update);
 
 
 
