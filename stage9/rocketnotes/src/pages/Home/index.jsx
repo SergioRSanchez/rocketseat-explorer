405accb1c3904ab1fbb6803d1fb3da9eec1d8c3b
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-01-15 20:49:24 -0300

    feat: handle tags selection

--- stage9/rocketnotes/src/pages/Home/index.jsx
+++ stage9/rocketnotes/src/pages/Home/index.jsx
@@ -14,6 +14,18 @@ import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 
 export function Home() {
   const [tags, setTags] = useState([]);
+  const [tagsSelected, setTagsSelected] = useState([]);
+
+  function handleTagSelected(tagName) {
+    const alreadySelected = tagsSelected.includes(tagName);
+    if (alreadySelected) {
+      const filteredTags = tagsSelected.filter(tag => tag !== tagName);
+      setTagsSelected(filteredTags);
+    } else {
+      setTagsSelected(prevState => [...prevState, tagName]);
+    }
+
+  }
 
   useEffect(() => {
     async function fetchTags() {
@@ -33,12 +45,20 @@ export function Home() {
       <Header />
 
       <Menu>
-        <li><ButtonText title="Todos" $isactive /></li>
+        <li>
+          <ButtonText
+            title="Todos"
+            $isactive={tagsSelected.length === 0}
+            onClick={() => handleTagSelected("all")}
+          />
+        </li>
         {
           tags && tags.map(tag => (
             <li key={String(tag.id)}>
               <ButtonText
                 title={tag.name}
+                onClick={() => handleTagSelected(tag.name)}
+                $isactive={tagsSelected.includes(tag.name)}
               />
             </li>
           ))
