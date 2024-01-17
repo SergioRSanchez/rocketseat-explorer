27eda528a444a8f1c8761b4c09328b8fd45f8f0e
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-01-16 21:03:46 -0300

    feat: config dotenv

--- stage8/rocketnotes/src/server.js
+++ stage8/rocketnotes/src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors");
+require("dotenv/config");
 
 const express = require("express");
 const routes = require("./routes");
@@ -30,7 +31,7 @@ app.use((error, request, response, next) => {
   })
 });
 
-const PORT = 3333;
+const PORT = process.env.SERVER_PORT || 3333;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}!`);
 })
